854f915f7b24fd98a71996ade724217f47ae21e1
Arjun Pandi <gmarjun100@gmail.com>
2024-09-04 18:51:00 +0530

    feat: appview init

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,15 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/RegularLayout.vue"),
-    children: [{ path: "", component: () => import("pages/web/Home.vue") }],
+    children: [{ path: "", component: () => import("src/pages/web/home.vue") }],
   },
-
+  {
+    path: "/app",
+    component: () => import("layouts/RegularLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/app/view/home.vue") },
+    ],
+  },
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
