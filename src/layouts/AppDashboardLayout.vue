af6aeb50bfbbd9d94e8614f1a9acd9087cc80beb
Arjun Pandi <gmarjun100@gmail.com>
2024-09-17 19:58:58 +0530

    fixed image issue

--- src/layouts/AppDashboardLayout.vue
+++ src/layouts/AppDashboardLayout.vue
@@ -6,24 +6,42 @@
 
     <q-footer unelevated class="bg-white text-primary">
       <q-toolbar class="justify-around">
-        <q-btn :ripple="false" unelevated flat round to="/app/dashboard">
-          <q-icon v-if="$route.path.includes('/app/dashboard')" size="27px" name="img:src/assets/icons/nav-home-active.svg" />
-          <q-icon v-else size="27px" name="img:src/assets/icons/nav-home.svg" />
+        <q-btn unelevated flat round to="/app/dashboard">
+          <q-icon v-if="$route.path.includes('/app/dashboard')" size="27px">
+            <img src="~src/assets/icons/nav-home-active.svg" alt="">
+          </q-icon>
+          <q-icon v-else size="27px">
+            <img src="~src/assets/icons/nav-home.svg" alt="">
+          </q-icon>
         </q-btn>
-        <q-btn :ripple="false" flat round to="/app/search">
-          <q-icon v-if="$route.path === '/app/search'" size="22px" name="img:src/assets/icons/nav-search-active.svg" />
-          <q-icon v-else size="22px" name="img:src/assets/icons/nav-search.svg" />
+        <q-btn flat round to="/app/search">
+          <q-icon v-if="$route.path === '/app/search'" size="22px">
+            <img src="~src/assets/icons/nav-search-active.svg" alt="">
+          </q-icon>
+          <q-icon v-else size="22px">
+            <img src="~src/assets/icons/nav-search.svg" alt="">
+          </q-icon>
+        </q-btn>    
+        <q-btn flat round to="/app/create" class="create-btn">
+          <q-icon size="32px">
+            <img src="~src/assets/icons/nav-create.svg" alt="">
+          </q-icon>
         </q-btn>
-        <q-btn :ripple="false" flat round to="/app/create" class="create-btn">
-          <q-icon size="32px" name="img:src/assets/icons/nav-create.svg" />
+        <q-btn flat round to="/app/profile">
+          <q-icon v-if="$route.path === '/app/profile'" size="24px">
+            <img src="~src/assets/icons/nav-user-active.svg" alt="">
+          </q-icon>
+          <q-icon v-else size="24px">
+            <img src="~src/assets/icons/nav-user.svg" alt="">
+          </q-icon>
         </q-btn>
-        <q-btn :ripple="false" flat round to="/app/profile">
-          <q-icon v-if="$route.path === '/app/profile'" size="24px" name="img:src/assets/icons/nav-user-active.svg" />
-          <q-icon v-else size="24px" name="img:src/assets/icons/nav-user.svg" />
-        </q-btn>
-        <q-btn :ripple="false" flat round to="/app/trending">
-          <q-icon v-if="$route.path === '/app/trending'" size="24px" name="img:src/assets/icons/nav-trending-up-active.svg" />
-          <q-icon v-else size="24px" name="img:src/assets/icons/nav-trending-up.svg" />
+        <q-btn flat round to="/app/trending">
+          <q-icon v-if="$route.path === '/app/trending'" size="24px">
+            <img src="~src/assets/icons/nav-trending-up-active.svg" alt="">
+          </q-icon>
+          <q-icon v-else size="24px">
+            <img src="~src/assets/icons/nav-trending-up.svg" alt="">
+          </q-icon>
         </q-btn>
       </q-toolbar>
     </q-footer>
