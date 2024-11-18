b6b043354c152fb042fdaa2b8bb93aa95a463ea1
vigneshwarankaliselvam <kvigneshwaran60@gmail.com>
2024-11-18 19:37:23 +0530

    rename-token

--- src/router/BeforeEach.js
+++ src/router/BeforeEach.js
@@ -2,7 +2,7 @@
 
 export default function (router) {
   router.beforeEach((to, from, next) => {
-    const isAuthenticated = !!localStorage.getItem("authToken");
+    const isAuthenticated = !!localStorage.getItem("access-token");
     const isMobile = window.innerWidth <= 768; // Example breakpoint for mobile screens
 
     if (to.meta.requiresAuth && !isAuthenticated) {
