e35e8506960599eebfc6b333296781502a7c3dc6
vigneshwaran k <kvigneshwaran60@gmail.com>
2025-01-03 08:53:39 +0530

    resolve-bugs

--- src/pages/app/view/dashboard/profile/profile.vue
+++ src/pages/app/view/dashboard/profile/profile.vue
@@ -253,9 +253,6 @@ onMounted(async () => {
 .q-page {
   display: grid;
   grid-template-rows: auto 1fr;
-  height: 100%;
-  overflow-y: auto; // Ensures vertical scrolling is enabled
-  -webkit-overflow-scrolling: touch;
   .q-tab-panels {
     height: 100%;
     min-height: 400px;
