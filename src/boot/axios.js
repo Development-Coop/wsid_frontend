5fb27caa0250fdab8813beed67f1de6efef7ab8d
Arjun Pandi <gmarjun100@gmail.com>
2025-04-27 02:10:45 +0530

    updated url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -61,7 +61,7 @@ export default boot(({ app, router }) => {
         try {
           // Attempt to refresh the token
           const refreshToken = localStorage.getItem("refresh-token");
-          const response = await axios.post("https://wsid-service.netlify.app/api/auth/refresh-token", {
+          const response = await axios.post(process.env.VUE_APP_API_URL + "/auth/refresh-token", {
             refreshToken,
           });
           // for testing in localhost
