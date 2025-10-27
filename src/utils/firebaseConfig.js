17822108c3a9b8e18889e9e9e24baaec22600422
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2025-10-26 23:31:04 -0600

    analytics

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -8,8 +8,9 @@ const firebaseConfig = {
   authDomain:
     process.env.VUE_APP_FB_AUTH_DOMAIN || "wsid-3b236.firebaseapp.com",
   projectId: process.env.VUE_APP_FB_PROJECT_ID || "wsid-3b236",
+    appId: "1:180937901593:web:f0ccea18856cc029d00c88",
     measurementId: "G-0DCP15K3SL",
-    
+
 };
 
 // Initialize Firebase App
